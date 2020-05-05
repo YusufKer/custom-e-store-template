7798ae40bf83c9b4959941d53dadbe896f610cf7
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-05-05 17:02:59 +0200

    refined navbar

--- src/components/re-use/Navbar.js
+++ src/components/re-use/Navbar.js
@@ -3,22 +3,15 @@ import {Link} from "react-router-dom"
 import "./navbar.css"
 
 function Navbar(){
-    const link_style={color:"black",textDecoration: "none"}
+    //some styling to remove the default css properties that come with react-router links
+    const link_style = {color:"black",textDecoration: "none"}
     //any values loaded into this array will act as links in the nav bar
-    const nav_links=["one","two","three","four","five"]
+    //the array is an array of objects which will need a name property and a link property
+    const nav_links = [{name:"one",link:""},{name:"two",link:"two"},{name:"three",link:"three"},{name:"four",link:"four"},{name:"five",link:"five"},]
+    const nav_items = nav_links.map(item => (<Link style={link_style} to={`/${item.link}`}>{item.name}</Link>))
     return(
         <div className="Navbar">
-            <ul>
-                {
-                    nav_links.map(item => (
-                        <Link 
-                            style={link_style} 
-                            to={`/${item}`}>
-                            {item}
-                        </Link>
-                    ))
-                }
-            </ul>
+            <ul>{nav_items}</ul>
         </div>
     )
 }
